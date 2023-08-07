555f547b52f84e1db12a2635c2ba2efc3fd06205
tusharbishttoshi <tusharbisht276@gmail.com>
2023-08-08 00:47:51 +0530

    creating header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import { StickyNavbar } from "./components/Header";
+
 function App(){
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <StickyNavbar/>
+  );
 }
 export default App;
